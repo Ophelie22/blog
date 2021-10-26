c35a007cae17834f1c723541ff30dfe6b87d3e48
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-10-26 19:00:50 +0200

    factorisation

--- index.php
+++ index.php
@@ -1,38 +1,22 @@
 <?php
-
 // Point d'entrée pour la page d'accueil
 
 // Inclusion des fichiers nécessaires
 require __DIR__ . '/inc/classes/Article.php';
-require __DIR__ . '/inc/data.php';
-
-// Récupération des données nécessaires la page (si besoin)
 
 
-
-// nous récupérons dans la variable $_GET['page'], la page que nous souhaitons afficher
-$page = filter_input(INPUT_GET, 'page');
-
-// déclaration de la variable $pageTemplate qui va nous permettre de savoir quel template doit être affiché
-
-// par défaut nous afficherons le template de la homepage si aucune page n'est spécifiée
-$pageTemplate = 'home.tpl.php';
-
-if($page === 'index') {
-    // si la page demandée est "index" ; nous voulons afficher le template
-    $pageTemplate = 'home.tpl.php';
-}
-elseif( $page === 'category') {
-    $pageTemplate = 'category.tpl.php';
-}
-elseif( $page === 'author') {
-    $pageTemplate = 'author.tpl.php';
-}
-elseif( $page === 'article') {
-    $pageTemplate = 'article.tpl.php';
+// Récupération des données nécessaires à la page (si besoin)
+// On récupère le paramètre page reçu en GET (dans l'url), s'il existe !
+// Avec ce if, on détermine la valeur de $requestedPage
+// $requestedPage existe toujours, et prend pour valeur, ce qui est dans le paramètre «page» dans l'url
+// Si l'url ne précise pas la page, alors la valeur de $requestedPage est "home"
+if (!empty($_GET['page'])) {
+    $requestedPage = $_GET['page'];
+} else {
+    $requestedPage = 'home';
 }
 
 // Affichage
-require __DIR__.'/inc/templates/header.tpl.php';
-require __DIR__.'/inc/templates/' . $pageTemplate;
-require __DIR__.'/inc/templates/footer.tpl.php';
+require __DIR__ . '/inc/templates/header.tpl.php';
+require __DIR__ . '/inc/templates/' . $requestedPage . '.tpl.php';
+require __DIR__ . '/inc/templates/footer.tpl.php';
