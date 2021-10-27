9c8559783f043cd58492ad9a82a37146c2f6ff2f
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-10-27 14:02:32 +0200

    ectif header avec integration du css et  du bootwrap

--- inc/templates/header.tpl.php
+++ inc/templates/header.tpl.php
@@ -10,6 +10,22 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,800" rel="stylesheet">
 
   <!-- Font-Awesome CSS -->
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
+    integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+  <!-- Ma feuille de style pour mon blog -->
+  <link rel="stylesheet" href="./css/blog.css">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+ <!-- HEADER -->
+ <header>
+  <!-- Font-Awesome CSS -->
   <nav class="navbar navbar-expand-md navbar-light">
       <!--
         Nous sommes en mobile first : par défaut notre menu est masqué !
